1f5390926ed11f12cf0695b0bddea0b56275e3d7
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-05-24 01:18:07 -0500

    Started making code generator

--- routes/routers.js
+++ routes/routers.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var con = require('../database');
+var randomstring = require("randomstring");
 var bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Start/quiniela.html');
   //res.render('index', { title: 'Express' });
+});
+
+router.get('/crear', function(req, res, next) {
+  
+  // This must go on the post request
+  con.query("SELECT code FROM quiniela", function(err, result) {
+    if (err) throw err;
+    var quiniela_codes = JSON.stringify(result);
+
+    while(true){
+      var new_code = randomstring.generate({
+        length: 5,
+        capitalization: 'uppercase'
+      });
+      console.log(new_code);
+      var repeated = false;
+      for(var code in quiniela_codes){
+        if(new_code === quiniela_codes[code]){
+          console.log('Repeated code');
+          repeated = true;
+          break;
+        }
+      }
+      if(!repeated){
+        // Insert to database
+        break;
+      }
+    }
+    
+    res.send("Query completada");
+  });
 });
 
 router.get('/login', function(req, res, next) {
