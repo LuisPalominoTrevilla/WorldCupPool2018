f4e30280c5bfd2842cf4df25bbe8dab5d4aaec49
Oscar Sierra <oscarsierra02@gmail.com>
2018-05-25 18:06:05 -0400

    -

--- public/Javascript/register.js
+++ public/Javascript/register.js
@@ -1,19 +1,46 @@
 var password, confirmPassword;
 var firstTime = true;
-var usernameValidation = false,
-    passwordValidation = false,
-    idQuinielaValidation = false;
+var userNameInDB, idQuinielaInDB;
+
+$(document).ready(function(){
+    askDataToDatabase();
+    if($("#username").val()!=""){
+        if(isUsernameAvailable()){
+            $("#usernameOnDatabase").html('');
+            $("#username").css('border-color','#ced4da');
+        }
+        else{
+            $("#usernameOnDatabase").html('<p class="text-muted" style="font-size:14px;">Ese nombre ya está siendo usado</p>');
+            $("#username").css('border-color','red');
+        }
+    }
+    if($("#idQuiniela").val()!=""){
+        if(isIdQuinielaInDatabase()){
+            $("#idQuinielaHelp").html('');
+            $("#idQuiniela").css('border-color','#ced4da');       
+        }
+        else{
+            $("#idQuinielaHelp").html('<p class="text-muted" style="font-size:14px;">Ese ID no existe </p>');
+            $("#idQuiniela").css('border-color','red');
+        }
+    }
+    buttonValidation();
+});
 
 //BUTTON CLICK
 $("#button").click(function(event){
-    if(!buttonValidation()){
+    askDataToDatabase();
+    if(buttonValidation()){
+       
+    }
+    else{
         event.preventDefault();
     }
 });
 
 //BUTTON VALIDATION
 function buttonValidation(){
-    if(usernameValidation==true && passwordValidation==true && idQuinielaValidation==true){
+    if(isUsernameAvailable() && isIdQuinielaInDatabase() && checkPassword()){
         $("#button").removeClass("disabled");
         $("#button").addClass("enabled");
         return true;
@@ -26,72 +53,103 @@ function buttonValidation(){
 }
 
 //USERNAME VALIDATION
-$("#username").focusout(function(){
-    $.getJSON('/api/users', function(data){
-        for(let object in data){
-            if($("#username").val() === data[object].username){
-               $("#usernameOnDatabase").html('<p class="text-muted" style="font-size:14px;">Ese nombre ya está siendo usado</p>');
-                $("#username").css('border-color','red');
-                usernameValidation=false;
-                buttonValidation();
-                return;
-            }
-        }
+$("#username").on("change keyup",function(){
+    if(isUsernameAvailable()){
         $("#usernameOnDatabase").html('');
         $("#username").css('border-color','#ced4da');
-        usernameValidation=true;
         buttonValidation();
-    });
-});
-
-//--PASSWORD AND CONFIRM PASSWORD VALIDATIONS--------------/
-$("#password").focusout(function(){
-    if(!firstTime){
-    checkPassword();    
+    }
+    else{
+        $("#usernameOnDatabase").html('<p class="text-muted" style="font-size:14px;">Ese nombre ya está siendo usado</p>');
+        $("#username").css('border-color','red');
+        buttonValidation();
     }
 });
 
-$("#confirm-password").focusout(function(){
+//--PASSWORD AND CONFIRM PASSWORD VALIDATIONS--------------/
+$("#password").on("change keyup",function(){
+    if(!firstTime){
+        if(checkPassword()){
+            $("#passwordsDontMatch").html('');
+            $("#password").css('border-color', '#ced4da');
+            $("#confirm-password").css('border-color', '#ced4da');
+            buttonValidation();
+        }else{
+            $("#passwordsDontMatch").html('<p class="text-muted" style="font-size:14px;">Las contraseñas no coinciden</p>');
+            $("#password").css('border-color', 'red');
+            $("#confirm-password").css('border-color', 'red');
+            buttonValidation();
+        }
+    }
+});
+
+$("#confirm-password").on("keyup change",function(){
     firstTime = false;
-    checkPassword();
+    if(checkPassword()){
+            $("#passwordsDontMatch").html('');
+            $("#password").css('border-color', '#ced4da');
+            $("#confirm-password").css('border-color', '#ced4da');
+        buttonValidation();
+        }else{
+            $("#passwordsDontMatch").html('<p class="text-muted" style="font-size:14px;">Las contraseñas no coinciden</p>');
+            $("#password").css('border-color', 'red');
+            $("#confirm-password").css('border-color', 'red');
+            buttonValidation();
+        }
 });
 //-------------------------------------------//
 
 //IDQUINIELA VALIDATION
-$("#idQuiniela").focusout(function(){
-    $.getJSON('/api/quinielas', function(data){
-        for(let object in data){
-            if($("#idQuiniela").val() == data[object].code){
-                $("#idQuinielaHelp").html('');
-                $("#idQuiniela").css('border-color','#ced4da');
-                idQuinielaValidation=true;
-                buttonValidation();
-                return;
-            }
-        }
+$("#idQuiniela").on("change keyup",function(event){
+    console.log(event);
+    if(isIdQuinielaInDatabase()){
+        $("#idQuinielaHelp").html('');
+        $("#idQuiniela").css('border-color','#ced4da');
+        buttonValidation();       
+    }
+    else{
         $("#idQuinielaHelp").html('<p class="text-muted" style="font-size:14px;">Ese ID no existe </p>');
         $("#idQuiniela").css('border-color','red');
-        idQuinielaValidation=false;
         buttonValidation();
-    });
+    }
+    
 });
 
 //CHECK PASSWORDS
 function checkPassword(){ 
     password = $("#password").val();
     confirmPassword = $("#confirm-password").val();
-    if(confirmPassword!=password){
-        $("#passwordsDontMatch").html('<p class="text-muted" style="font-size:14px;">Las contraseñas no coinciden</p>');
-        $("#password").css('border-color', 'red');
-        $("#confirm-password").css('border-color', 'red');
-        passwordValidation=false;
-        buttonValidation();
+    if(confirmPassword!=password || (confirmPassword=="" || password=="")){
+        return false;
     }else{
-        $("#passwordsDontMatch").html('');
-        $("#password").css('border-color', '#ced4da');
-        $("#confirm-password").css('border-color', '#ced4da');
-        passwordValidation=true;
-        buttonValidation();
+        return true;
     }
+}
+
+function isIdQuinielaInDatabase(){
+    for(let object in idQuinielaInDB){
+        if($("#idQuiniela").val() == idQuinielaInDB[object].code && $("#idQuiniela").val() != ""){
+            return true;
+        }
+    }
+    return false;
+}
+
+function isUsernameAvailable(){
+    for(let object in userNameInDB){
+        if($("#username").val() === userNameInDB[object].username || $("#username").val() == ""){
+            return false;
+        }
+    }    
+    return true;
+}
+
+function askDataToDatabase(){
+    $.getJSON('api/users', function(data){
+        userNameInDB = data;
+    });
+    $.getJSON('/api/quinielas', function(data){
+        idQuinielaInDB = data;
+    });
 }
 
