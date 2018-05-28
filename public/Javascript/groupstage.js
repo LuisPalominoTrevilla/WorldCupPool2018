81e8531bc3dbf25196af3d47f3271a83a53e09c0
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-05-28 00:33:27 -0500

    Started designing matches and bets

--- public/Javascript/groupstage.js
+++ public/Javascript/groupstage.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 
     // Get group stage matches and bets for the user
-    $.getJSON('/api/matches/1', function(data){
+    /* $.getJSON('/api/matches/1', function(data){
         console.log(data);
-    });
+    }); */
+
+    var groupTemplate = new Vue({
+        el: "#matches_bets",
+        data: {
+            predicted_result: null
+        },
+        methods: {
+            add: function(group){
+                //this.groups.push(group);
+            }
+        }
+    })
 });
